6bd588957a8b7564b057f324db234b86b4605a7c
Rodrigo Ferreira Rodrigues <104102666+rfrodriguesBra96@users.noreply.github.com>
2022-08-08 22:18:33 -0300

    Update

--- weather_observation_station_6.sql
+++ weather_observation_station_6.sql
@@ -3,3 +3,9 @@ select distinct city from station where city like 'A%' or
                                         city like 'I%' or
                                         city like 'O%' or
                                         city like 'U%';
+
+-- Using regexp_like
+-- ^[aeiou] -> a|A or e|E or i|I or o|O or u|U
+select distinct city
+from station
+where regexp_like(city, '^[aeiou]', 'i');
