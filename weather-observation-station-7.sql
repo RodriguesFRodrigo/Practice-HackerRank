8325774e5f25c12e1d0fec07f101222bed06d16b
Rodrigo Ferreira Rodrigues <104102666+rfrodriguesBra96@users.noreply.github.com>
2022-08-08 22:15:34 -0300

    Update regexp_like and i

--- weather-observation-station-7.sql
+++ weather-observation-station-7.sql
@@ -5,3 +5,15 @@ where city like '%a' or
       city like '%i' or
       city like '%o' or
       city like '%u';
+
+-- Using regexp_like
+select distinct city
+from station
+where regexp_like(city, '[aAeEiIoOuU]$', 'i');
+
+-- Using regexp_like and i lower and upper
+-- [aeiou] -> a or e or i or o or u
+-- i -> aA, eE, iI, oO, uU
+select distinct city
+from station
+where regexp_like(city, '[aeiou]$', 'i');
